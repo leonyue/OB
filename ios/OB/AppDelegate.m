d2cccd0bf034c2b153582e5cc35e01edbded6469
得建岳 <dejianyue@dejianyuedeMacBook-Pro.local>
2017-09-10 11:00:15 +0800

    添加定位

--- ios/OB/AppDelegate.m
+++ ios/OB/AppDelegate.m
@@ -11,6 +11,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface AppDelegate ()<CLLocationManagerDelegate>
+
+@property (nonatomic, strong) CLLocationManager* locationManager;
+
+@end
 
 @implementation AppDelegate
 
@@ -31,7 +38,55 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+//  CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+  self.locationManager = [[CLLocationManager alloc] init];
+  self.locationManager.delegate = self;
+  self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  [self findMe];
+  
   return YES;
+}
+
+- (void)findMe
+{
+  /** 由于IOS8中定位的授权机制改变 需要进行手动授权
+   * 获取授权认证，两个方法：
+   * [self.locationManager requestWhenInUseAuthorization];
+   * [self.locationManager requestAlwaysAuthorization];
+   */
+  if ([self.locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
+    NSLog(@"requestAlwaysAuthorization");
+    [self.locationManager requestAlwaysAuthorization];
+  }
+  else if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+    NSLog(@"requestWhenInUseAuthorization");
+    [self.locationManager requestWhenInUseAuthorization];
+  }
+  
+  //开始定位，不断调用其代理方法
+  [self.locationManager startUpdatingLocation];
+  NSLog(@"start gps");
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray *)locations
+{
+  // 1.获取用户位置的对象
+  CLLocation *location = [locations lastObject];
+  CLLocationCoordinate2D coordinate = location.coordinate;
+  NSLog(@"纬度:%f 经度:%f", coordinate.latitude, coordinate.longitude);
+  
+  // 2.停止定位
+  [manager stopUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error
+{
+  if (error.code == kCLErrorDenied) {
+    // 提示用户出错原因，可按住Option键点击 KCLErrorDenied的查看更多出错信息，可打印error.code值查找原因所在
+  }
 }
 
 @end
